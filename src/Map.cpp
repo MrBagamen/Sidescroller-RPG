194a87ca2f5236de7c769f2f0a4b1e3477773a87
Mika Attila <radiantstatue@gmail.com>
2013-10-23 14:32:59 +0200

    Map::tileAt: Have range checking also check for underflow

--- src/Map.cpp
+++ src/Map.cpp
@@ -23,7 +23,7 @@ void Map::resize(int width, int height)
 Tile& Map::tileAt(int x, int y)
 {
 #ifdef SSRPG_DEBUG
-    assert(x < m_width && y < m_height);
+    assert(x >= 0 && x < m_width && y >= 0 && y < m_height);
 #endif
     return m_tiles[y * m_width + x];
 }
@@ -31,7 +31,7 @@ Tile& Map::tileAt(int x, int y)
 const Tile &Map::tileAt(int x, int y) const
 {
 #ifdef SSRPG_DEBUG
-    assert(x < m_width && y < m_height);
+    assert(x >= 0 && x < m_width && y >= 0 && y < m_height);
 #endif
     return m_tiles[y * m_width + x];
 }
